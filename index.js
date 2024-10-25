a20b5847c63612ff361af9504acaf63db4b84a4b
rchesaux <chesaux66@gmail.com>
2024-10-25 10:29:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('check');
+
+const images = [
+    'trickortreat/images/images (1).jpeg',
+    'trickortreat/images/images (2).jpeg',
+    'trickortreat/images/images.jpeg',
+    'trickortreat/images/images (3).jpeg',
+    'trickortreat/images/images (4).jpeg',
+]
+
+function showCandy() {
+    
+}
